30b6140904c9bf5f83ee866cbcaafcec9cb14fc4
EnochQin <qyqdream@gmail.com>
2020-05-26 16:02:39 +0800

    React + Redux

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  add1() {
+    this.props.store.dispatch({
+      type: "add",
+      payload: 1
+    })
+  }
+  minus1() {
+    this.props.store.dispatch({
+      type: "minus",
+      payload: 1
+    })
+  }
+  add1IfOdd() {
+    this.props.store.dispatch({
+      type: "addIfOdd",
+      payload: 1
+    })
+  }
+  add1Async() {
+    setTimeout(() => {
+      this.props.store.dispatch({
+        type: "add",
+        payload: 1
+      })
+    }, 1000)
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello React x Redux</h1>
+        <div>
+          <p>你点击了 {this.props.store.getState()} 次</p>
+          <button onClick={this.add1.bind(this)}>+1</button>
+          <button onClick={this.minus1.bind(this)}>-1</button>
+          <button onClick={this.add1IfOdd.bind(this)}>数字为单数，+1</button>
+          <button onClick={this.add1Async.bind(this)}>1秒后+1</button>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,13 +3,36 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from "redux"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const store = createStore((state, action) => {
+  state = state || 0
+  switch (action.type) {
+    case "add":
+      return state + action.payload
+    case "minus":
+      return state - action.payload
+    case "addIfOdd":
+      if (state % 2 === 1) {
+        return state + action.payload
+      } else {
+        return state
+      }
+    default:
+      return state
+  }
+})
+
+render()
+function render() {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App store={store} />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
+store.subscribe(render)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
