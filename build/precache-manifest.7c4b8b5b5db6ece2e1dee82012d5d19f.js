8021dce2a6e8df9458ad8494fab5bab7caa6a944
EnochQin <qyqdream@gmail.com>
2020-05-30 23:26:07 +0800

    add build pages

---
+++ build/precache-manifest.7c4b8b5b5db6ece2e1dee82012d5d19f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad7e702bed3ad0969c16dbbc984a3133",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae85b640300b48040e18",
+    "url": "/static/css/main.f8669387.chunk.css"
+  },
+  {
+    "revision": "01745b11cfd4fe5c46dc",
+    "url": "/static/js/2.6b7b0346.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6b7b0346.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae85b640300b48040e18",
+    "url": "/static/js/main.afd30b05.chunk.js"
+  },
+  {
+    "revision": "93cb2bbd3d52813af6db",
+    "url": "/static/js/runtime-main.8fcb26cc.js"
+  }
+]);
